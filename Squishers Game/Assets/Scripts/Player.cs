a2c92771fe57125ded313544685a602fa31b659d
BTP <liamcalicegamedev@gmail.com>
2018-10-02 10:15:04 +0200

    Initial Commit
    initial commint test !

---
+++ Squishers Game/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public GameObject projectile;
+
+    private void Update()
+    {
+
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+
+                Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, 90));
+                timeBtwAttack = startTimeBtwAttack;
+            }
+            else if(Input.GetKeyDown(KeyCode.Q))
+            {
+                Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, 180));
+                timeBtwAttack = startTimeBtwAttack;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, 0));
+                timeBtwAttack = startTimeBtwAttack;
+            }
+
+            
+        }
+        else {
+            timeBtwAttack -= Time.deltaTime;
+        }
+        
+    }
+}
---
+++ Squishers Game/Assets/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public GameObject projectile;
+    public Transform arrowPos;
+    private float timeBtwShot;
+    public float startTimeBtwShot;
+
+    public float offset;
+
+    private Transform player;
+    public float speed;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Vector2 pos = new Vector2(player.position.x, player.position.y + 1.9f);
+        transform.position = Vector2.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShot <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, arrowPos.position, transform.rotation);
+            }
+        }
+        else {
+            timeBtwShot -= Time.deltaTime;
+        }
+        
+    }
+}
